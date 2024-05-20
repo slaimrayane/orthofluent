71a015adc9400e08c13a0a4b2049ea7f753ceaa0
Slaim Rayane <mr_slaim@esi.dz>
2024-05-20 22:43:47 +0100

    test

--- src/main/java/com/orthofluent/orthofluent/HelloController.java
+++ src/main/java/com/orthofluent/orthofluent/HelloController.java
@@ -1,14 +1,19 @@
 package com.orthofluent.orthofluent;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+
+import javafx.event.ActionEvent;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void up(ActionEvent event) {
+        System.out.println("up");
+    }
+        public void down(ActionEvent event) {
+        System.out.println("down");
+    }
+        public void left(ActionEvent event) {
+        System.out.println("left");
+    }
+        public void right(ActionEvent event) {
+        System.out.println("right");
     }
 }
