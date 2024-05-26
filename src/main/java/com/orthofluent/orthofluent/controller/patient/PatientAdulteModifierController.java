326ceaa2d3392361a2fd303b810db3242b8563cd
Slaim Rayane <mr_slaim@esi.dz>
2024-05-26 01:26:23 +0100

    interface 1.7.5 mise en relation entre les interfaces et les controllers

---
+++ src/main/java/com/orthofluent/orthofluent/controller/patient/PatientAdulteModifierController.java
@@ -0,0 +1,6 @@
+package com.orthofluent.orthofluent.controller.patient;
+
+import com.orthofluent.orthofluent.controller.BaseController;
+
+public class PatientAdulteModifierController extends BaseController {
+}
---
+++ src/main/java/com/orthofluent/orthofluent/controller/patient/PatientEnfantModifierController.java
@@ -0,0 +1,7 @@
+package com.orthofluent.orthofluent.controller.patient;
+
+import com.orthofluent.orthofluent.controller.BaseController;
+
+public class PatientEnfantModifierController extends BaseController {
+
+}
